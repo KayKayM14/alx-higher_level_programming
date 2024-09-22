990e244e38bd955f55901f361c3f7b1cf364287c
KayKayM14 <kayleighkeciamartin@gmail.com>
2024-09-22 10:34:45 -0700

    0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,10 @@
-#include "LIST_H"
+#include "lists.h"
 
 /**
+ * print_dlistint - Prints all the elements of a dlistint_t list.
+ * @h: Pointer to the head of the list.
+ *
+ * Return: The number of nodes in the list.
  */
 
 {
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,25 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dlistint_s - double linked list
+ * @n: integer stored in a node
+ * @prev: previous node
+ * @next: next node 
+ */
+
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistin_s *next;
+}dlistint_t;
+
+
 size_t print_dlistint(const dlistint_t *h);
+
 
 #endif
